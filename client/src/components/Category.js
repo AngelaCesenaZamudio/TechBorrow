eaf3c733a33f3df82d0e0c1156a388ec4624fbbb
AngelaCesenaZamudio <cesena.angela@uabc.edu.mx>
2024-04-26 18:31:45 -0700

    Cambio2

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,23 +1,34 @@
 import React, {Component} from 'react';
 import MenuItem from '../MenuItem.js';
+import './Category.css';
 
 class Category extends Component {
     constructor(props){
         super(props);
         this.state = {visible:false}
     }
+
+    handleClick(event){
+      event.preventDefault();
+      this.setState({
+        visible:!this.state.visible
+      })
+    }
+
   render() {  
   return (
+    <header>
     <div className="Category">
     <li>
-        <h3>{this.props.name}</h3>
-        <ul>
+        <h3 onClick={this.handleClick.bind(this)}>{this.props.name}</h3>
+        <ul className={this.state.visible?'visible':'no-visible'}>
           {this.props.items.map((item)=>{
             return <MenuItem name ={item} key={item}/>
           })}
         </ul>
     </li>
     </div>
+    </header>
   );
 }
 }
