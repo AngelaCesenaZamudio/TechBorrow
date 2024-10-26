9da6c2e5cc330dc69f6189a195641bfe4ead0ee5
AngelaCesenaZamudio <125494890+AngelaCesenaZamudio@users.noreply.github.com>
2024-10-25 21:55:33 -0700

    Modificacion-Menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,6 +10,7 @@ function Header(){
     const [dropdown2, setDropdown2]=useState(false);
     const [dropdown3, setDropdown3]=useState(false);
     const [dropdown4, setDropdown4]=useState(false);
+    const [dropdown5, setDropdown5]=useState(false);
 
     const toggleDropdown2=()=>{
         setDropdown2(prevState=>!prevState);
@@ -20,42 +21,61 @@ function Header(){
     const toggleDropdown4=()=>{
         setDropdown4(prevState=>!prevState);
     };
+    const toggleDropdown5=()=>{
+        setDropdown5(prevState=>!prevState);
+    };
 
     return (
         <div className='bg-white'>
         <div className='md:px-10 py-0 px-7 md:flex justify-between items-center'>
+            <div className='bg-green-600 p-1 w-auto rounded-md'>
             <div className='flex text-2xl cursor-pointer items-center gap-2'>
-            <img src='./src/imagenes/escudo.png' width={40} height={40}></img>
-                <span className='font-bold text-lg'>Laboratorio de Sistemas Computacionales.</span>
+            <img src='./src/imagenes/logo-cimarron.png' width={40} height={40}></img>
+                <span className='font-bold text-lg text-white'>Laboratorio de Sistemas Computacionales.</span>
+            </div>
             </div>
 
-            <div className='flex space-x-16 text-xl'>
+            <div className='flex space-x-4 text-xl'>
             <button className='border-black text-black'><a href='/Menu'>Menu</a></button>
 
             <Dropdown isOpen={dropdown2} toggle={toggleDropdown2}>
                 <DropdownToggle className='border-transparent bg-white text-black'>Servicios</DropdownToggle>
             
             <DropdownMenu>
-                 <DropdownItem><a href="./Prestamo">Registro Prestamo</a></DropdownItem>
-                 <DropdownItem><a href="">Registro Devolucion</a></DropdownItem>
-                 <DropdownItem>Consulta Prestamos</DropdownItem>
+                 <DropdownItem><a href="./Prestamo">Prestamo</a></DropdownItem>
+                 <DropdownItem><a href="">Devolucion</a></DropdownItem>
+                 <DropdownItem>Reporte</DropdownItem>
+                 <DropdownItem>Inventario</DropdownItem>
             </DropdownMenu>
             </Dropdown>
 
             <Dropdown isOpen={dropdown3} toggle={toggleDropdown3}>
                 <DropdownToggle className='border-transparent bg-white text-black'>Material</DropdownToggle>
+            <DropdownMenu>
+                            <DropdownItem toggle={false} onClick={toggleDropdown4} className='text-black'>
+                                Registrar
+                            </DropdownItem>
+
+                            {dropdown4 && (
+                                <div className='border-transparent bg-white shadow-lg mt-2 ml-3 p-2'>
+                                    <DropdownMenu>
+                                        <DropdownItem>Material</DropdownItem>
+                                        <DropdownItem>Marca</DropdownItem>
+                                    </DropdownMenu>
+                                </div>
+                            )}
+
+                 <DropdownItem>Consultar</DropdownItem>
+                 <DropdownItem>Modificar</DropdownItem>
+                 <DropdownItem>Eliminar</DropdownItem>
+            </DropdownMenu>
+            </Dropdown>
+
+            <Dropdown isOpen={dropdown5} toggle={toggleDropdown5}>
+                <DropdownToggle className='border-transparent bg-white text-black'>Usuario</DropdownToggle>
             
             <DropdownMenu>
-            <Dropdown isOpen={dropdown4} toggle={toggleDropdown4}>
-                <DropdownToggle className='border-transparent bg-white text-black'>Registrar</DropdownToggle>
-                   <DropdownMenu>
-                   <DropdownItem>Material</DropdownItem>
-                   </DropdownMenu>
-                   </Dropdown>
-
-                 <DropdownItem>Consulta Material</DropdownItem>
-                 <DropdownItem>Modificacion Material</DropdownItem>
-                 <DropdownItem>Eliminar Material</DropdownItem>
+                 <DropdownItem><a href="./Prestamo">Registrar</a></DropdownItem>
             </DropdownMenu>
             </Dropdown>
             
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -4,8 +4,7 @@ import Footer from './footer.jsx';
 function Menu() {
   return (
     <div>
-      {/* Renderiza el componente Header */}
-      
+    
     </div>
   );
 }
