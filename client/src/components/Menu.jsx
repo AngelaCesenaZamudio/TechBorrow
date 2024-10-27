50b3e76526ec5d792634d8fafce9ffef6edb0812
AngelaCesenaZamudio <125494890+AngelaCesenaZamudio@users.noreply.github.com>
2024-10-26 22:22:11 -0700

    Menu
    Correcion entre la conexion de pantallas

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,7 +1,9 @@
-import Header from './Header.jsx'; // Importa el componente Header
-import Footer from './footer.jsx'; 
+import { useNavigate } from "react-router-dom";
 
 function Menu() {
+
+  const navigate =useNavigate();
+
   return (
     <div className='bg-white text-xl font-bold max-w-7xl mx-auto p-4'>
      <h1 className='mb-4'>Sistema para el material de laboratorio</h1>
@@ -9,7 +11,8 @@ function Menu() {
      <div className="flex items-right justify-center mb-4">
      <div className='flex w-full'>
      <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
-        <button className="border-4 border-slate-7000/50 text-black p-2.5 rounded-lg flex-grow flex items-center transition duration-200">
+        <button className="border-4 border-slate-7000/50 text-black p-2.5 rounded-lg flex-grow flex items-center transition duration-200"
+         onClick={() => navigate('')}>
           <div className='bg-zinc-100'>
           <img src='./src/imagenes/material.png' className="w-15 h-15 mr-4 transform-rotate-15" />
           </div>
@@ -24,7 +27,24 @@ function Menu() {
     <div className="flex items-right justify-center mb-4">
      <div className='flex w-full'>
      <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
-        <button className="border-4 border-slate-7000/50 text-black p-2.5 rounded-lg flex-grow flex items-center transition duration-200">
+        <button className="border-4 border-slate-7000/50 text-black p-2.5 rounded-lg flex-grow flex items-center transition duration-200"
+         onClick={() => navigate('/MenuServicios')}>
+          <div className='bg-zinc-100'>
+          <img src='./src/imagenes/servicios.png' className="w-15 h-15 mr-4 transform-rotate-15" />
+          </div>
+          <span className="font-bold tranform -translate-y-6">Servicios</span>
+          <span className='ml-0 text-xs text-gray-600'>
+            Prestamo | Devolucion | Reportes | Inventarios 
+          </span>
+        </button>
+      </div>
+    </div>
+    
+    <div className="flex items-right justify-center mb-4">
+     <div className='flex w-full'>
+     <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
+        <button className="border-4 border-slate-7000/50 text-black p-2.5 rounded-lg flex-grow flex items-center transition duration-200"
+         onClick={() => navigate('')}>
           <div className='bg-zinc-100'>
           <img src='./src/imagenes/usuario.png' className="w-15 h-15 mr-4 transform-rotate-15" />
           </div>
@@ -36,20 +56,7 @@ function Menu() {
       </div>
     </div>
 
-    <div className="flex items-right justify-center mb-4">
-     <div className='flex w-full'>
-     <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
-        <button className="border-4 border-slate-7000/50 text-black p-2.5 rounded-lg flex-grow flex items-center transition duration-200">
-          <div className='bg-zinc-100'>
-          <img src='./src/imagenes/servicios.png' className="w-15 h-15 mr-4 transform-rotate-15" />
-          </div>
-          <span className="font-bold tranform -translate-y-6">Servicios</span>
-          <span className='ml-0 text-xs text-gray-600'>
-            Registrar | Consultar | Modificar | Eliminar 
-          </span>
-        </button>
-      </div>
-    </div>
+    
   </div>
   );
 }
---
+++ client/src/components/MenuServicios.jsx
@@ -0,0 +1,79 @@
+import { useNavigate } from "react-router-dom";
+
+function MenuServicios() {
+
+  const navigate = useNavigate();
+
+  return (
+    <div className='bg-white text-xl font-bold max-w-7xl mx-auto p-4'>
+     <h1 className='mb-4'>Servicios</h1>
+
+     <div className="flex items-right justify-center mb-1">
+     <div className='flex w-full'>
+     <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
+        <button className="border-4 border-slate-7000/50 text-black p-2 rounded-lg flex-grow flex items-center transition duration-200"
+        onClick={() => navigate('/Prestamo')}>
+          <div className='bg-zinc-100'>
+          <img src='./src/imagenes/servicios.png' className="w-13 h-13 mr-3 transform-rotate-15" />
+          </div>
+          <span className="font-bold tranform -translate-y-6">Prestamos</span>
+          <span className='ml-0 text-xs text-gray-600'>
+            Registrar | Consultar 
+          </span>
+        </button>
+      </div>
+    </div>
+
+    <div className="flex items-right justify-center mb-1">
+     <div className='flex w-full'>
+     <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
+        <button className="border-4 border-slate-7000/50 text-black p-2 rounded-lg flex-grow flex items-center transition duration-200"
+         onClick={() => navigate('')}>
+          <div className='bg-zinc-100'>
+          <img src='./src/imagenes/devolucion.png' className="w-13 h-13 mr-3 transform-rotate-15" />
+          </div>
+          <span className="font-bold tranform -translate-y-6">Devolucion</span>
+          <span className='ml-0 text-xs text-gray-600'>
+            Registrar | Consultar 
+          </span>
+        </button>
+      </div>
+    </div>
+
+    <div className="flex items-right justify-center mb-1">
+     <div className='flex w-full'>
+     <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
+        <button className="border-4 border-slate-7000/50 text-black p-1 rounded-lg flex-grow flex items-center transition duration-200"
+         onClick={() => navigate('')}>
+          <div className='bg-zinc-100'>
+          <img src='./src/imagenes/reportes.png' className="w-13 h-13 mr-3 transform-rotate-15" />
+          </div>
+          <span className="font-bold tranform -translate-y-6">Reportes</span>
+          <span className='ml-0 text-xs text-gray-600'>
+            Registrar | Consultar 
+          </span>
+        </button>
+      </div>
+    </div>
+
+    <div className="flex items-right justify-center mb-1">
+     <div className='flex w-full'>
+     <div className='bg-green-700 w-2 h-full rounded-l-md'></div>
+        <button className="border-4 border-slate-7000/50 text-black p-2 rounded-lg flex-grow flex items-center transition duration-200"
+         onClick={() => navigate('/MenuServicios')}>
+          <div className='bg-zinc-100'>
+          <img src='./src/imagenes/inventario.png' className="w-13 h-13 mr-3 transform-rotate-15" />
+          </div>
+          <span className="font-bold tranform -translate-y-6">Inventario</span>
+          <span className='ml-0 text-xs text-gray-600'>
+            Registrar | Consultar 
+          </span>
+        </button>
+      </div>
+    </div>
+  </div>
+  
+  );
+}
+
+export default MenuServicios;
